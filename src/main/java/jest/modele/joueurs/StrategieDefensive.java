5a77d14fcee37097a4f9f66d0a63b0cea320b637
Ayat <ayatatraoui@icloud.com>
2025-12-16 16:32:13 +0100

    Fix: ignore bin and target, remove compiled files

---
+++ src/main/java/jest/modele/joueurs/StrategieDefensive.java
@@ -0,0 +1,14 @@
+package jest.modele.joueurs;
+
+import jest.modele.cartes.Carte;
+import jest.modele.jeu.Offre;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Stratégie défensive : tous les choix sont faits au hasard.
+ * Utilisation basique
+ */
+public class StrategieDefensive{
+
+}
---
+++ src/main/java/jest/modele/joueurs/StrategieGloutonne.java
@@ -0,0 +1,44 @@
+package jest.modele.joueurs;
+
+import jest.modele.cartes.Carte;
+import jest.modele.jeu.Offre;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+/**
+ * Stratégie gloutonne : tous les choix sont la au hasard.
+ * Utilisation basique
+ */
+public class StrategieGloutonne implements StrategieJeu{
+	
+	
+	public StrategieGloutonne() {
+    }
+    
+	
+    @Override
+    public Carte choisirCarteOffre(List<Carte> main, Jest jest) {
+        return main.stream()
+                .min(Comparator.comparingInt(Carte::getValeurFaciale))
+                .orElse(main.get(0));
+    }
+    
+    
+    @Override
+    public Offre choisirOffreCible(List<Offre> offres, Jest jest) {
+        Offre offreMax = offres.get(0);
+        for (Offre offre : offres) {
+            if (offre.getCarteVisible().getValeurFaciale() > offreMax.getCarteVisible().getValeurFaciale()) {
+                offreMax = offre;
+            }
+        }
+        return offreMax;  
+    }
+    
+    @Override
+    public Carte choisirCarteDansOffre(Offre offre, Jest jest) {
+        return offre.getCarteVisible();
+    }
+}
+
